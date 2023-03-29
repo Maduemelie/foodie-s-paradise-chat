7b393f6a92fe46c1836e3f7c73b9558de03b025d
Madu Chiemelie Precious <101350287+Maduemelie@users.noreply.github.com>
2023-03-30 00:36:22 +0100

    modifying my html

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 const express = require("express");
-const connectToDb = require("./config/mongoDb");
+const app = express();
+const server = require("http").Server(app);
 const cors = require("cors");
-const sessionMiddleWare = require('./config/session')
+const io = require("socket.io")(server);
+
+const connectToDb = require("./config/mongoDb");
+const sessionMiddleWare = require("./config/session");
 const mealRouter = require("./routes/mealPlanRoutes");
 const foodRouter = require("./routes/foodRoutes");
 const orderRouter = require("./routes/orderRouter");
-const { generateUserId } = require("./utils/userId");
+// const { generateUserId } = require("./utils/userId");
 
 require("dotenv").config();
-const app = express();
+// const app = express();
 
 const PORT = process.env.PORT || 4000;
-
 
 app.use(cors());
 app.use(sessionMiddleWare);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
-app.use("/api/v1/mealplan", mealRouter);
-app.use("/api/v1/food", foodRouter);
-app.use("/api/v1/order", orderRouter);
+// app.use("/api/v1/mealplan", mealRouter);
+// app.use("/api/v1/food", foodRouter);
+// app.use("/api/v1/order", orderRouter);
 
 app.get("/Foodie's_Paradise", (req, res) => {
-  // let userId = generateUserId(); // function to generate a unique user ID
-  console.log(req.session.id)
-  const userId = (req.sessionID)
-  
-  res.cookie('userId', userId); // set the userId cookie
   res.sendFile(__dirname + "/public/index.html");
 });
-
+io.on('connection', (socket) => {
+  console.log(socket.id)
+})
 /** catch 404 and forward to error handler */
-app.use('*', (req, res) => {
+app.use("*", (req, res) => {
   return res.status(404).json({
     success: false,
-    message: 'API endpoint doesnt exist'
-  })
+    message: "API endpoint doesnt exist",
+  });
 });
 connectToDb();
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,116 @@
+import store from "./store.js"
+import ui from "./ui.js"
+
+const nameInput = document.querySelector(".name_input");
+nameInput.addEventListener("keyup", (event) => {
+  store.setUsername(event.target.value);
+});
+
+const enterChat = document.getElementById("enter_chat_button");
+
+enterChat.addEventListener("click", (e) => {
+  ui.foodiesChatPage()
+  // socketHandler.connectToSocketIoServer();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const output = document.querySelector(".output");
 const Foodie_chat = document.querySelector(".Foodie_chat");
 const inputNum = document.getElementById("inputNum");
