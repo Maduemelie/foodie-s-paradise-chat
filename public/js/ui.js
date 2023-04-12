7b1df06dbdeaa3a6cd942a9ef2e978835f30c9c8
Madu Chiemelie Precious <101350287+Maduemelie@users.noreply.github.com>
2023-04-12 23:18:34 +0100

    appendOptionData

--- public/js/ui.js
+++ public/js/ui.js
@@ -65,7 +65,7 @@ const appendOptionsData = (optionData) => {
   const selectedFoods = optionData.map(
     (food) => `</br>${food.name}:  ${food.price}`
   );
-  const foodList = `<h4>You selected the following food items:</h4> ${selectedFoods}.`;
+  const foodList = `<h4>You selected the following food items:</h4> ${selectedFoods}. </br></br> Input 99 to checkout your order.`;
 
   const data = {
     author: "Foodie's-Bot",
